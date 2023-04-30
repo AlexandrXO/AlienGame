52dfb27405056df879f23acfbeb58e0445a0f8ee
XO <cawahotina@gmail.com>
2023-04-30 18:07:36 +0300

    add

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,14 +1,17 @@
 import sys
 import pygame
-
+from settings import Settings
 class AlienInvasion:
     #управление ресурсами и поведение
 
     def __init__(self):
 
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+
         pygame.display.set_caption('Alien Invasion')
         #nazna4enie cveta
         self.bg_color = (230, 230, 230)
@@ -20,6 +23,8 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             self.screen.fill(self.bg_color)
+
+            self.screen.fill(self.settings.bg_color)
 
         pygame.display.flip()
 
