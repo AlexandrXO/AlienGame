0b5ef654d38d66c9fd66d8eef4053f23b8b4e3ad
XO <cawahotina@gmail.com>
2023-04-30 21:33:07 +0300

    add

--- ship.py
+++ ship.py
@@ -1,18 +1,17 @@
 import pygame
-class Ship():
+class Ship:
     """Класс для управления кораблем."""
 
     def __init__(self, ai_game):
         """Инициализирует корабль и задает его начальную позицию."""
 
-        self.screen = ai_game.screen(Ship)
+        self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
-# Загружает изображение корабля и получает прямоугольник.
-        self.image = pygame.image.load('images/ship.bmp')
-        self.rect = self.image.get_rect()
-# Каждый новый корабль появляется у нижнего края экрана.
-        self.rect.midbottom = self.screen_rect.midbottom
 
+        self.image = pygame.image.load('images/ship.bmp.bmp')
+        self.rect = self.image.get_rect()
+
+        self.rect.midbottom = self.screen_rect.midbottom
 
     def blitme(self):
         """Рисует корабль в текущей позиции."""
