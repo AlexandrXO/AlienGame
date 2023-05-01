74405f0b81e9f0eaffcc0fd09c6725a4219035f9
XO <cawahotina@gmail.com>
2023-05-01 11:53:11 +0300

    add

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,8 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from  bullet import Bullet
+
 class AlienInvasion:
     """Класс для управления ресурсами и поведением игры."""
     def __init__(self):
@@ -21,15 +23,21 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
-# Назначение цвета фона.
+        self.bullets = pygame.sprite.Group()
+
+        # Назначение цвета фона.
         self.bg_color = (230, 230, 230)
+
     def run_game(self):
         """Запуск основного цикла игры."""
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
             # При каждом проходе цикла перерисовывается экран.
+
     def _check_events(self):
         '''Обрабатывает нажатия клавиш и события мыши.'''
 # Отслеживание событий клавиатуры и мыши.
@@ -48,6 +56,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
 
     def _check_keyup_events(self, event):
@@ -56,14 +66,29 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """Создание нового снаряда и включение его в группу bullets."""
+        if len(self.bullets) < self.settings.bullet_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Обновляет позиции снарядов и уничтожает старые снаряды."""
+        # Обновление позиций снарядов.
+        self.bullets.update()
+        # Удаление снарядов, вышедших за край экрана.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _update_screen(self):
         '''Обновляет изображения на экране и отображает новый экран.'''
 # При каждом проходе цикла перерисовывается экран.
         self.screen.fill(self.bg_color)
         self.screen.fill(self.settings.bg_color)
-
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         pygame.display.flip()
 
 if __name__ == '__main__':
